aa5cb9995aeb1bfb8b533fae2ae628a79680b54c
alexmagnan <1062886@imac-P322-11.local>
2015-09-28 09:54:56 -0400

    Added Toast
    Made a toast to button Press me

--- GitDemo/app/src/main/java/com/cs616/gitdemo/MainActivity.java
+++ GitDemo/app/src/main/java/com/cs616/gitdemo/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private Button pressButton;
@@ -13,6 +15,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pressButton = (Button) findViewById(R.id.pressMeButton);
+
+        pressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "A Toast",Toast.LENGTH_LONG);
+
+            }
+        });
     }
 
     @Override
